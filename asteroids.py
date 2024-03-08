d2ea79a92267707e111b079c294de7bbfc88bdd3
juanx199 <jccarvajalcamargo@gmail.com>
2024-03-07 20:17:26 -0500

    casi finalizado

--- asteroids.py
+++ asteroids.py
@@ -1,34 +1,87 @@
-import pygame
+import sys, pygame, random
 from pygame.locals import *
-import sys
 from ship import *
+from asteroid import *
+from bullet import *
 
 size = width, height = 800 , 600
 
 screen = pygame.display.set_mode(size)
 
+
 def main():
     pygame.init()
+    pygame.mixer.init()
+    pygame.mixer.music.load("sonidos/sonido.mp3")
+    pygame.mixer.music.play(1)
 
     background_image = pygame.image.load("imagenes\space.jpeg")
     background_rect = background_image.get_rect()
 
-    pygame.display.set_caption("Asteroids")
+    pygame.display.set_caption("asteroids")
 
     ship = Ship(size)
 
+    asteroids = []
 
+    
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-                
-        ship.update()
+        
+        pygame.font.init()
         screen.blit(background_image, background_rect)
+        fuente = pygame.font.Font(None,45)
+        texto_puntos = fuente.render("Puntos:" + str(ship.puntos),1,(250,250,250))
+        texto_vida = fuente.render("Vida: " + str(ship.vida),1,(250,250,250))
+        fuente_go = pygame.font.Font(None,100)
+        texto_fin = fuente_go.render("FIN DEL JUEGO ",1,(250,0,0))
+
+
+        ship.update()
+
+        if random.randint(0,100) % 25 == 0 and len(asteroids) < 10:
+            new_asteroid = Asteroid(size)
+            asteroids.append(new_asteroid)
+
+        
+        for bullet in ship.bullets:
+            bullet.update()
+            if bullet.alcance == 0:
+                ship.bullets.remove(bullet)
+                screen.blit(bullet.image, bullet.rect)
         screen.blit(ship.imagen, ship.rect)
+
+        for asteroid in asteroids:
+            asteroid.update()
+            screen.blit(asteroid.image, asteroid.rect)
+            for bullet in ship.bullets:
+                if asteroid.rect.colliderect(bullet.rect):
+                    ship.bullets.remove(bullet)
+                    ship.puntos += 1
+                    if asteroid in asteroids:
+                        asteroid.explotar()
+                        screen.blit(asteroid.image, asteroid.rect)
+                        asteroids.remove(asteroid)
+        
+            if ship.rect.colliderect(asteroid.rect):
+                ship.vida -= 10
+                if asteroid in asteroids:
+                        asteroid.explotar()
+                        screen.blit(asteroid.image, asteroid.rect)
+                        asteroids.remove(asteroid)        
+        if ship.vida > 0:
+            screen.blit(texto_vida,(600,50))
+            screen.blit(texto_puntos,(100,50))
+        else:
+            screen.blit(texto_fin,(150,250))
+            
+        screen.blit(ship.imagen, ship.rect)
+
 
         pygame.display.update()
         pygame.time.delay(10)
 
-if __name__ == "__main__" :
+if __name__ == '__main__': 
     main()
--- ship.py
+++ ship.py
@@ -2,18 +2,26 @@ import pygame
 from pygame.locals import *
 from pygame.sprite import Sprite
 import math
+from bullet import *
 
 class Ship(Sprite):
     def __init__(self, contenedor):
         self.puntos = 0
         self.angulo = 0
+        self.radio = 8
         self.vida = 100
         self.velocidad = [0, 0]
+        self.bullets = []
         self.contenedor = contenedor
         self.imagen_base = pygame.image.load("imagenes/nave.jpeg")
         self.imagen = self.imagen_base
         self.rect = self.imagen.get_rect()
         self.rect.move_ip(contenedor[0]/2, contenedor[1]/2)
+        self.impulso = pygame.mixer.Sound('sonidos/impulso.mp3')
+        self.impulso.set_volume(0.05)
+        self.disparo = pygame.mixer.Sound('sonidos/disparo.mp3')
+        self.disparo.set_volume(1)
+
 
     def update(self):
         teclas = pygame.key.get_pressed()
@@ -24,6 +32,8 @@ class Ship(Sprite):
             self.rotar(-2)
         elif teclas[K_UP]:
             self.acelerar()
+        elif teclas[K_SPACE]:
+            self.disparar()
         
         self.velocidad[0] *= 0.99
         self.velocidad[1] *= 0.99
@@ -32,8 +42,17 @@ class Ship(Sprite):
         self.rect.x %= self.contenedor[0]
         self.rect.y %= self.contenedor[1]
          
+    def disparar(self):
+        self.disparo.play()
+        vector = [0,0]
+        vector[0] += math.cos(math.radians((self.angulo)%360))
+        vector[1] -= math.sin(math.radians((self.angulo)%360))
+        pos = [self.rect.x + self.radio, self.rect.y + self.radio]
+        vel = [self.velocidad[0] + 6 * vector[0], self.velocidad[1] + 6 * vector[1]]
+        self.bullets.append(Bullet(pos,self.angulo,vel, self.contenedor))
 
     def acelerar(self):
+        self.impulso.play()
         self.velocidad[0] += math.cos(math.radians((self.angulo)%360))
         self.velocidad[1] -= math.sin(math.radians((self.angulo)%360))
 
